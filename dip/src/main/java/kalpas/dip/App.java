37829c49d11b6a646e4865d7537d70e073d7767f
andrey.arnautov <puffdrew@gmail.com>
2012-04-10 00:15:05 +0300

    formatting
    Signed-off-by: andrey.arnautov <puffdrew@gmail.com>

--- dip/src/main/java/kalpas/dip/App.java
+++ dip/src/main/java/kalpas/dip/App.java
@@ -4,9 +4,11 @@ package kalpas.dip;
  * Hello world!
  * 
  */
-public class App {
-	public static void main(String[] args) {
+public class App
+{
+    public static void main(String[] args)
+    {
 
-		System.out.println("Hello World!");
-	}
+        System.out.println("Hello World!");
+    }
 }
